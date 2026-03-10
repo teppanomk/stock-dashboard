1dea8f5d96b986fa29c254e8ab33cde7fdde242b
teppanomk <teppanom@siit.tu.ac.th>
2026-03-10 10:47:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -66,23 +66,21 @@ Question: ${question}
 
     try{
 
-        const res = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
-        {
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({
-                contents:[
-                    {
-                        parts:[
-                            {text: prompt}
-                        ]
-                    }
-                ]
-            })
-        });
+ const response = await fetch(
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
+{
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json"
+  },
+  body:JSON.stringify({
+    contents:[
+      {
+        parts:[{text:prompt}]
+      }
+    ]
+  })
+});
 
         const data = await res.json();
 
